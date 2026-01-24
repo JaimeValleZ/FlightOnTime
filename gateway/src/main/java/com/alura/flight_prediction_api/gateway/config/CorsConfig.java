a3b70ea582e839efce9eee56224c2c6b65f9fee0
JaimeValleZ <jaime.valle@cun.edu.co>
2026-01-24 00:16:40 -0500

    cors para docker

--- gateway/src/main/java/com/alura/flight_prediction_api/gateway/config/CorsConfig.java
+++ gateway/src/main/java/com/alura/flight_prediction_api/gateway/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedOrigin("https://flightontime-frontend.onrender.com");
+        config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
