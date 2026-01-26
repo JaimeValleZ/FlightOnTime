3c9228e45c1a58e778e5de6abf709c56e06ddf17
JaimeValleZ <jaime.valle@cun.edu.co>
2026-01-25 20:44:36 -0500

    leer variable de entorno

--- gateway/src/main/java/com/alura/flight_prediction_api/gateway/config/CorsConfig.java
+++ gateway/src/main/java/com/alura/flight_prediction_api/gateway/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.alura.flight_prediction_api.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,11 +10,14 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
+    @Value("${FRONT_END}")
+    private String frontEndUrl;
+
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedOrigin("${FRONT_END}");
+        config.addAllowedOrigin(frontEndUrl);
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
